277094f953fc9257f70db535243ac8bece56fbf7
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-02-14 17:21:50 +0200

    request uzhnu with user-agent

--- lab1.py
+++ lab1.py
@@ -2,6 +2,9 @@ from requests import get
 
 
 URL = "https://www.uzhnu.edu.ua/uk/cat/faculty"
-page = get(URL)
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+}
+page = get(URL, headers=HEADERS)
 
 print (page.text)
