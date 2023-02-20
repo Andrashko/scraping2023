91b2c0b48e980558cc5f6beee875aff08c6e0723
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-02-20 18:48:19 +0200

    scrapy faculty parse

--- lab2/lab2/spiders/uzhnu.py
+++ lab2/lab2/spiders/uzhnu.py
@@ -1,10 +1,21 @@
 import scrapy
+from bs4 import BeautifulSoup
 
 
 class UzhnuSpider(scrapy.Spider):
     name = "uzhnu"
     allowed_domains = ["uzhnu.edu.ua"]
-    start_urls = ["http://uzhnu.edu.ua/"]
+    start_urls = ["https://uzhnu.edu.ua/uk/cat/faculty"]
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.body,  "html.parser")
+        fac_list = soup.find(class_="departments_unfolded")
+        for li in fac_list.find_all("li"):
+            a = li.find("a")
+            fac_name = a.find(string=True, recursive=False)
+            fac_link = "https://uzhnu.edu.ua/" + a.get("href")
+            yield {
+                "name": fac_name,
+                "URL": fac_link
+            }
+
