0e9725b818b338cdfb2bc25b785a760821c243c8
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-02-24 18:50:03 +0200

    hotline pages parse

--- lab1_2.py
+++ lab1_2.py
@@ -7,20 +7,24 @@ URL = f"{BASE_URL}/ua/computer/noutbuki-netbuki/"
 HEADERS = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
 }
+LAST_PAGE = 443
 
-page = get(URL, headers=HEADERS)
-soup = BeautifulSoup(page.content,  "html.parser")
+for p in range(1, LAST_PAGE):
+    #надсилаємо параметр сторінки
+    page = get(URL, headers=HEADERS, params={"p": p})
+    soup = BeautifulSoup(page.content,  "html.parser")
 
-# знаходимо список товарів
-items = soup.find(
-    name="div", class_="list-body__content").find_all(class_="list-item")
-# Для кожного товару
-for item in items:
-    # Знаходимо назву
-    title = item.find(
-        name="a", class_="list-item__title").find(string=True, recursive=False).strip()
-    # та ціну
-    price = item.find(class_="price__value").find(string=True, recursive=False)
+    # знаходимо список товарів
+    items = soup.find(
+        name="div", class_="list-body__content").find_all(class_="list-item")
+    # Для кожного товару
+    for item in items:
+        # Знаходимо назву
+        title = item.find(
+            name="a", class_="list-item__title").find(string=True, recursive=False).strip()
+        # та ціну
+        price = item.find(class_="price__value").find(
+            string=True, recursive=False)
 
-    print(f"Назва: {title}")
-    print(f"Ціна: {price}")
+        print(f"Назва: {title}")
+        print(f"Ціна: {price}")
