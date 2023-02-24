62e05a53dfdc5e6cc9edab02c9cfe29f32b2ea72
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-02-24 18:40:23 +0200

    hotline parse

---
+++ lab1_2.py
@@ -0,0 +1,26 @@
+from requests import get
+from bs4 import BeautifulSoup
+
+
+BASE_URL = "https://hotline.ua"
+URL = f"{BASE_URL}/ua/computer/noutbuki-netbuki/"
+HEADERS = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36"
+}
+
+page = get(URL, headers=HEADERS)
+soup = BeautifulSoup(page.content,  "html.parser")
+
+# знаходимо список товарів
+items = soup.find(
+    name="div", class_="list-body__content").find_all(class_="list-item")
+# Для кожного товару
+for item in items:
+    # Знаходимо назву
+    title = item.find(
+        name="a", class_="list-item__title").find(string=True, recursive=False).strip()
+    # та ціну
+    price = item.find(class_="price__value").find(string=True, recursive=False)
+
+    print(f"Назва: {title}")
+    print(f"Ціна: {price}")
