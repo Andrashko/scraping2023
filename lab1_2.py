28965fa450e2fdc8b8eb31473f1bd7ec5d046077
Yurii Andrashko <yurii.andrashko@uznhu.edu.ua>
2023-02-24 18:58:29 +0200

    hotline to file

--- lab1_2.py
+++ lab1_2.py
@@ -9,22 +9,27 @@ HEADERS = {
 }
 LAST_PAGE = 443
 
-for p in range(1, LAST_PAGE):
-    #надсилаємо параметр сторінки
-    page = get(URL, headers=HEADERS, params={"p": p})
-    soup = BeautifulSoup(page.content,  "html.parser")
+# відкриваємо файл
+FILE_NAME = "laptops.txt"
+with open(FILE_NAME, "w", encoding="utf-8") as file:
+    for p in range(1, LAST_PAGE):
+        # надсилаємо параметр сторінки
+        page = get(URL, headers=HEADERS, params={"p": p})
+        soup = BeautifulSoup(page.content,  "html.parser")
 
-    # знаходимо список товарів
-    items = soup.find(
-        name="div", class_="list-body__content").find_all(class_="list-item")
-    # Для кожного товару
-    for item in items:
-        # Знаходимо назву
-        title = item.find(
-            name="a", class_="list-item__title").find(string=True, recursive=False).strip()
-        # та ціну
-        price = item.find(class_="price__value").find(
-            string=True, recursive=False)
+        # знаходимо список товарів
+        items = soup.find(
+            name="div", class_="list-body__content").find_all(class_="list-item")
+        # Для кожного товару
+        for item in items:
+            # Знаходимо назву
+            title = item.find(
+                name="a", class_="list-item__title").find(string=True, recursive=False).strip()
+            # та ціну
+            price = item.find(class_="price__value").find(
+                string=True, recursive=False)
 
-        print(f"Назва: {title}")
-        print(f"Ціна: {price}")
+            print(f"Назва: {title}")
+            print(f"Ціна: {price}")
+            file.write(f"Назва: {title}\n")
+            file.write(f"Ціна: {price}\n")
